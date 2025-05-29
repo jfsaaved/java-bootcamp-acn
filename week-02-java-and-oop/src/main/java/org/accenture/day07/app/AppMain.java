c2957d5fb69cd1ce571b16246ecc4c17e9b43ec2
jfsaaved-accenture <julian.saavedra@accenture.com>
2025-05-28 22:40:26 -0400

    some stuff

--- week-02-java-and-oop/src/main/java/org/accenture/day07/app/AppMain.java
+++ week-02-java-and-oop/src/main/java/org/accenture/day07/app/AppMain.java
@@ -16,7 +16,29 @@ import static org.accenture.day07.util.RandomGenerator.RANDOM;
 public class AppMain {
 
     public static void main(String[] args) throws InterruptedException {
+        bootUp();
         simulateRun();
+    }
+
+    private static void bootUp() throws InterruptedException {
+        System.out.println("  .   ____          _            __ _ _");
+        System.out.println(" /\\\\ / ___'_ __ _ _(_)_ __  __ _ \\ \\ \\ \\");
+        System.out.println("( ( )\\___ | '_ | '_| | '_ \\/ _` | \\ \\ \\ \\");
+        System.out.println(" \\\\/  ___)| |_)| | | | | || (_| |  ) ) ) )");
+        System.out.println("  '  |____| .__|_| |_|_| |_\\__, | / / / /");
+        System.out.println(" =========|_|==============|___/=/_/_/_/");
+        System.out.println(" :: Spring Boot :: (mock version)");
+
+        Thread.sleep(400); // simulate delay
+
+        Logger log = new Logger(AppMain.class);
+        log.info("Starting DemoApplication using Java 17.0.9");
+        Thread.sleep(200);
+        log.info("No active profile set, falling back to default profiles: default");
+        Thread.sleep(200);
+        log.info("EmbeddedWebServer initialized on port 8080");
+        Thread.sleep(300);
+        log.info("Application started in 1.230 seconds (JVM running for 1.451)");
     }
 
     private static void simulateRun() throws InterruptedException {
@@ -25,9 +47,9 @@ public class AppMain {
         Data01Service data01Service = new Data01Service(messageService);
         Data01Controller data01Controller = new Data01Controller(data01Service);
 
-        Logger log = new Logger(Data01Controller.class);
+        Logger log = new Logger(AppMain.class);
 
-        for (int i = 0; i < 100; i++) {
+        while(true) {
             Data01 incomingData = createData01();
             Status result = data01Controller.putMethod(incomingData);
             Thread.sleep(RANDOM.nextInt(500));
